e6590aa00c9827e1d4487c245d62b005599db1e5
UMASHIBA <umashiba1@gmail.com>
2020-09-25 18:44:05 +0900

    add: index.cssでグローバルのスタイルを定義

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import Home from "./Home";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
