37b5e3f7c6ad019be86b9cd6e745f6b4fd5c82c9
UMASHIBA <umashiba1@gmail.com>
2020-09-25 18:09:13 +0900

    set: いらないファイルを削除

---
+++ src/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  width: 100%;
+`;
+const Home = () => {
+  return (
+    <Wrapper>
+      {/* dd */}
+      {/* dd */}
+    </Wrapper>
+  );
+};
+
+export default Home;
